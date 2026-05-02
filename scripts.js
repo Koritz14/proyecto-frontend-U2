5ea9106e98f833718387dbe4f728e53ad78f5bd7
Mauro Gonzalez <mauro.soto1475@gmail.com>
2026-05-02 16:10:42 -0400

    Validaciones de formulario

--- scripts.js
+++ scripts.js
@@ -39,3 +39,56 @@ function mostrarServicios() {
 }
 
 mostrarServicios();
+
+function validarFormulario() {
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const celular = document.getElementById("celular").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const pais = document.getElementById("pais").value.trim();
+    const ciudad = document.getElementById("ciudad").value.trim();
+    const consulta = document.getElementById("consulta").value.trim();
+
+    const mensaje = document.getElementById("mensaje");
+
+    mensaje.classList.remove("error", "exito");
+
+    // VALIDACIÓN 1: campos vacíos
+    if (!nombre || !celular || !email || !pais || !ciudad || !consulta) {
+        mensaje.textContent = "Todos los campos son obligatorios";
+        mensaje.classList.add("error"); 
+        return;
+    }
+
+    // VALIDACIÓN 2: email válido
+    if (!email.includes("@") || !email.includes(".")) {
+        mensaje.textContent = "El email no es válido";
+        mensaje.classList.add("error"); 
+        return;
+    }
+
+    // VALIDACIÓN 3: celular numérico
+    if (isNaN(celular) || celular.length < 8 || celular.length > 9) {
+    mensaje.textContent = "El celular debe tener entre 8 y 9 números";
+    mensaje.classList.add("error");
+    return;
+}
+
+    // VALIDACIÓN 4: largo mínimo de consulta
+    if (consulta.length < 5) {
+        mensaje.textContent = "La consulta debe tener al menos 5 caracteres";
+        mensaje.classList.add("error"); 
+        return;
+    }
+
+    // SI TODO ESTÁ BIEN
+    mensaje.textContent = "Formulario enviado correctamente";
+    mensaje.classList.add("exito"); 
+}
+
+document.getElementById("formulario").addEventListener("submit", function(e) {
+    e.preventDefault(); //  evita que la página se recargue
+
+    validarFormulario();
+});
+
